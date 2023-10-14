11abae8c86f6dc11b238f1e85fd388a23700c47c
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2023-10-14 17:25:28 +0200

    b4q: Initial TWRP device tree Made with SebaUbuntu's TWRP device tree generator Arch: arm64 Manufacturer: samsung Model: SM-F721B Script version: 3.0.0

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2023 The Android Open Source Project
+# Copyright (C) 2023 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_b4q.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_b4q-user \
+    omni_b4q-userdebug \
+    omni_b4q-eng
